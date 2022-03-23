ce8039be0f3b38d6326ea28cfa212918aef5fc8e
Edwin Sam <edwinsamjr@gmail.com>
2022-03-22 22:58:40 -0400

    Adding Swagger/OpenAPI Dependency

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/ApplicationController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/ApplicationController.java
@@ -7,6 +7,8 @@ import com.techelevator.tenmo.exceptions.*;
 import com.techelevator.tenmo.model.Transfer;
 import com.techelevator.tenmo.model.TransferDetails;
 import com.techelevator.tenmo.model.User;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -32,7 +34,8 @@ public class ApplicationController {
         this.userDao = userDao;
     }
 
-    @GetMapping(path = "/balance")
+    @GetMapping (path = "/balance")
+    @Operation (summary = "Returns current user's balance")
     public BigDecimal findBalance(Principal principal) {
         //Gets current user's balance
         return this.transferDao.getBalance(principal.getName());
@@ -40,6 +43,7 @@ public class ApplicationController {
 
 
     @GetMapping(path = "/users")
+    @Operation (summary = "Returns list who you can send money to")
     public List<User> listUsers(Principal principal) {
         //Lists users besides current user
 
@@ -50,6 +54,7 @@ public class ApplicationController {
 
 
     @GetMapping(path = "/viewtransfers")
+    @Operation (summary = "Returns all completed incoming or outgoing transfers for current user")
     public List<Transfer> viewTransfers(Principal principal) {
         //Lists all transfers for current user
 
@@ -59,6 +64,7 @@ public class ApplicationController {
     }
 
     @GetMapping(path = "/viewtransferdetails")
+    @Operation (summary = "Returns current user's transfer with additional details")
     public List<TransferDetails> viewTransferDetails(Principal principal) {
         //Lists all transfers with details for current user
 
@@ -68,6 +74,7 @@ public class ApplicationController {
     }
 
     @GetMapping(path = "/viewtransfers/{id}")
+    @Operation (summary = "Gets transfer by transferId")
     public Transfer viewTransferById(Principal principal, @PathVariable int id) throws TransferNotFoundException {
 
         Transfer transfer = this.transferDao.findTransferById(principal.getName(), id);
@@ -75,6 +82,7 @@ public class ApplicationController {
     }
 
     @GetMapping(path = "/viewtransferdetails/{id}")
+    @Operation (summary = "Gets transfer by transferId with additional details")
     public TransferDetails viewTransferDetailsById(Principal principal, @PathVariable int id) throws TransferNotFoundException {
 
         TransferDetails transferDetails = this.transferDetailsDao.findTransferById(principal.getName(), id);
@@ -83,6 +91,7 @@ public class ApplicationController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(path = "/send")
+    @Operation (summary = "Send money to another user")
     public void addTransfer(Principal principal, @Valid @RequestBody Transfer transfer) throws InvalidTransferException, AccountNotFoundException {
         //Enters Transfer into database
 
@@ -91,6 +100,7 @@ public class ApplicationController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(path = "/request")
+    @Operation (summary = "Request money from another user")
     public void request(Principal principal, @Valid @RequestBody Transfer transfer) throws InvalidTransferException {
 
         this.transferDao.request(transfer, principal.getName());
@@ -98,27 +108,32 @@ public class ApplicationController {
 
 
     @GetMapping(path = "/viewrequests")
+    @Operation (summary = "View current user's pending requests")
     public List<Transfer> viewRequests(Principal principal) {
         return transferDao.viewRequests(principal.getName());
     }
 
     @GetMapping(path = "/listrequests")
+    @Operation (summary = "View current user's pending requests with additional details")
     public List<TransferDetails> listRequests(Principal principal) {
         return transferDetailsDao.listRequests(principal.getName());
     }
 
 
     @PostMapping(path = "/approve/{id}")
+    @Operation (summary = "Approve a transfer request by transferId")
     public void approveRequest(Principal principal, @PathVariable int id) throws InvalidTransferException {
         this.transferDao.approveRequest(id, principal.getName());
     }
 
     @PostMapping(path = "/reject/{id}")
+    @Operation (summary = "Reject a transfer request by transferId")
     public void rejectRequest(Principal principal, @PathVariable int id) throws IllegalTranscationException {
         this.transferDao.rejectRequest(id, principal.getName());
     }
 
     @GetMapping(path = "/user")
+    @Operation (summary = "Returns current logged in user")
     public User getUser(Principal principal) {
         //Gets current user
 
@@ -126,15 +141,8 @@ public class ApplicationController {
         return user;
     }
 
-
-//    @GetMapping(path = "/username/{id}")
-//    public String getUsernameByAccountId(Principal principal, @PathVariable int id) {
-//
-//        String username = transferDao.getUsernameByAccountId(id);
-//        return username;
-//    }
-
     @GetMapping(path = "/account")
+    @Operation (summary = "Returns current user's accountId")
     public int getAccountId(Principal principal) {
         //Gets current user's account Id
 
@@ -144,6 +152,7 @@ public class ApplicationController {
 
 
     @GetMapping(path = "/account/{username}")
+    @Operation (summary = "Retrieves accountId by username")
     public int getAccountIdByUsername(@PathVariable String username) {
         int accountId = transferDao.getUserAccountId(username);
         return accountId;
